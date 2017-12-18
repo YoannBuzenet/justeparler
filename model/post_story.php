f2e8a19e2fe016b7304b0af099c8914727086ccb
Yoann Buzenet <ybuzenet@gmail.com>
2017-12-18 01:26:32 +0100

    Beginning of the temporary posts when not logued/registered. Still have to add the transfer if logging, and the destroy.

--- model/post_story.php
+++ model/post_story.php
@@ -1,2 +1,14 @@
-<?php $req = $bdd->prepare('INSERT INTO posts (texte, auteur, titre_post, categorie, timepost) VALUES(?, ?, ?, ?, NOW())');
+<?php 
+
+session_start();
+
+if(isset($_SESSION['pseudo'])){ 
+$req = $bdd->prepare('INSERT INTO posts (texte, auteur, titre_post, categorie, timepost) VALUES(?, ?, ?, ?, NOW())');
 $req->execute(array($_POST['msg'], $_POST['pseudo'], $_POST['titre_post'], $_POST['categorie']));
+}
+else{
+$req2 = $bdd->prepare('INSERT INTO temporary_posts (temporary_text, temporary_author, temporary_title, temporary_category, id_session, date_temporary_post) VALUES(?, ?, ?, ?, ?, NOW())');
+$req2->execute(array($_POST['msg'], $_POST['pseudo'], $_POST['titre_post'], $_POST['categorie'], session_id()));
+
+}
+
