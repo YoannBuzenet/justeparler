a30717596d4f389ffe93c079f28121a9afdd32f7
Yoann Buzenet <ybuzenet@gmail.com>
2018-01-08 15:20:40 +0100

    Added the responsive rule for smartphone. Can now begin to make it better.

--- view/main_view.php
+++ view/main_view.php
@@ -6,6 +6,7 @@
 		<link rel="stylesheet" type="text/css" href="theme.css" />
 		<title> Parler</title>
 		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width" />
 	</head>
 
 <body>
